7de898541da7670b83246f78713f86caf64b12ea
codewars <cr8her@gmail.com>
2021-10-17 15:38:44 -0400

    Case Swapping 7kyu

---
+++ case-swapping/main.js
@@ -0,0 +1,24 @@
+// Given a string, swap the case for each of the letters.
+
+// e.g. CodEwArs --> cODeWaRS
+
+// Examples
+// ""           ->   ""
+// "CodeWars"   ->   "cODEwARS"
+// "abc"        ->   "ABC"
+// "ABC"        ->   "abc"
+// "123235"     ->   "123235"
+
+
+function swap(str){
+    let changeCase = ""
+    
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === str[i].toLowerCase()) {
+        changeCase += str[i].toUpperCase()
+      } else {
+        changeCase += str[i].toLowerCase()
+      }
+    }
+    return changeCase
+  }
